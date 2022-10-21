5f8afb5307d2facf7fe5cc9cf2aa77d15398f31d
Jan Król <janek01krol@gmail.com>
2022-10-21 15:01:05 +0200

    dodalem pesel feature2

--- app/Konto.py
+++ app/Konto.py
@@ -1,3 +1,6 @@
 class Konto:
-    def __init__(self):
-        pass
+    def __init__(self, imie, nazwisko, pesel):
+        self.imie = imie
+        self.nazwisko = nazwisko
+        self.saldo = 0
+        self.pesel = pesel
