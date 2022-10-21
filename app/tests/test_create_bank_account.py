6d15269e1728358f596bf91ed6bab8afcc097c9f
Jan Król <janek01krol@gmail.com>
2022-10-21 15:25:21 +0200

    pesel zamieniony na string

--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -3,11 +3,13 @@ import unittest
 from ..Konto import Konto
 
 class TestCreateBankAccount(unittest.TestCase):
-
+    imie = "darek"
+    pesel = "38497392043"
     def test_tworzenie_konta(self):
-        pierwsze_konto = Konto("Dariusz", "Januszewski", 38497392043)
-        self.assertEqual(pierwsze_konto.imie, "Dariusz", "Imie nie zostało zapisane!")
+        pierwsze_konto = Konto(self.imie, "Januszewski", self.pesel)
+        self.assertEqual(pierwsze_konto.imie, self.imie, "Imie nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.nazwisko, "Januszewski", "Nazwisko nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.saldo, 0, "Saldo nie jest zerowe!")
-        self.assertEqual(pierwsze_konto.pesel, 38497392043, "PESEL nie został podany!")
+        self.assertEqual(pierwsze_konto.pesel, self.pesel, "PESEL nie został podany!")
+        
     #tutaj proszę dodawać nowe testy
