0c9be955d4d9446313925f3581b5a16b86080205
Jan123Krol <janek01krol@gmail.com>
2022-10-23 12:01:37 +0200

    pesel

--- app/Konto.py
+++ app/Konto.py
@@ -1,5 +1,6 @@
 class Konto:
-    def __init__(self, imie: str, nazwisko: str) -> None:
+    def __init__(self, imie: str, nazwisko: str, pesel: str) -> None:
         self.imie: str = imie
         self.nazwisko: str = nazwisko
         self.saldo: int = 0
+        self.pesel: str = pesel
--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -8,9 +8,10 @@ class TestCreateBankAccount(unittest.TestCase):
         self.imie = "darek"
 
     def test_tworzenie_konta(self):
-        pierwsze_konto = Konto(self.imie, "Januszewski")
+        pierwsze_konto = Konto(self.imie, "Januszewski", "68042385996")
         self.assertEqual(pierwsze_konto.imie, self.imie, "Imie nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.nazwisko, "Januszewski", "Nazwisko nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.saldo, 0, "Saldo nie jest zerowe!")
+        self.assertEqual(pierwsze_konto.pesel, "68042385996", "Numer pesel nie został zapisany!")
 
     #tutaj proszę dodawać nowe testy
