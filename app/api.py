1f34bf9ff07fb31fa5df79e117a1fe85df86f4b8
Jan Król <janek01krol@gmail.com>
2022-11-25 15:26:22 +0100

    flask

---
+++ app/api.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, jsonify
+from app.Konto import (Konto, RejestrKont)
+
+
+
+app = Flask(__name__)
+
+@app.route("konta/stworz_konto", methods=['POST'])
+def stworz_konto():
+    dane = request.get_json()
+    print(f"Request o stworzenie konta z danymi: {dane}")
+    konto = Konto(dane["imie"], dane["nazwisko"], dane["pesel"])
+    RejestrKont.dodaj_konto(konto)
+    return jsonify("Konto stworzone"), 201
